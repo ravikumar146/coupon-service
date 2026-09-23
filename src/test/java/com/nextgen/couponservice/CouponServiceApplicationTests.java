4592729691750e2fc5c2fdce89a6b1060d34c308
Ravi Kumar <botcha.rav146@gmail.com>
2026-09-23 13:03:15 +0530

    Updated sql connection details

--- src/test/java/com/nextgen/couponservice/CouponServiceApplicationTests.java
+++ src/test/java/com/nextgen/couponservice/CouponServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.nextgen.couponservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CouponServiceApplicationTests {
 
     @Test
